25dc50de27e52f3b76980a7ce14959a2421d440a
UBONGO2000 <gadyubongo@gmail.com>
2025-08-29 11:13:59 +0200

    Amélioration de l'affichage du post sur la page d'administration

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Post
+
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','slug','author','published','status']
+    list_filter = ['status','created','published','author']
+    search_fields = ['title','body']
+    # Pour le remplissage automatique du champ 'slug' en utilisant une version slugifier du champ title
+    prepopulated_fields = {'slug':('title',)}
+    # Affiche un champ de saisie d’ID brut (plutôt qu’un menu déroulant) pour le champ ForeignKey “author”.
+    raw_id_fields = ['author']
+    date_hierarchy = 'published'
+    ordering = ['status','published']
+    # Affiche les differents nombres deveant les filtres 
+    show_facets=admin.ShowFacets.ALWAYS
+    
+    
