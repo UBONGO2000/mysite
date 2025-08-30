33ea8d2f99d27cc3f10551e40f25104d7199f9f3
UBONGO2000 <gadyubongo@gmail.com>
2025-08-30 21:33:09 +0200

    ajout de la pagination, avec gestion erreur

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 
 from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.all()
+    post_list = Post.publish.all()
+    paginator=Paginator(post_list,3)
+    page_number=request.GET.get('page',1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Si page_number n'est pas un nombre entier, récupérer la première page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
+    
     return render(request,'blog/post/list.html',{'posts':posts})
 
 def post_detail(request,year,month,day,post):
