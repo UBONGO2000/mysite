380e7171d5f14edd04de2a123eefe97f2011428d
UBONGO2000 <gadyubongo@gmail.com>
2025-08-30 10:57:16 +0200

    insertion du css

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,24 +1,41 @@
+from django.urls import reverse
 from django.utils import timezone
 from django.db import models
 from django.conf import settings
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT='DF','Draft'
         PUBLISHED = 'PB','publish'
+    
+    objects = models.Manager() # manager par default
+    publish = PublishedManager() #notre manager customiser
+    
     title = models.CharField(max_length=255)
+    
     # Champ slug utilisé souvent pour les URLs, limité à 250 caractères.
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,unique_for_date='published')
+    
     body = models.TextField()
-# Date et heure de publication, par défaut la date et heure actuelle au moment de la création.
+    
+    # Date et heure de publication, par défaut la date 
+    # et heure actuelle au moment de la création.
     published = models.DateTimeField(default=timezone.now)
-# Date et heure de création du post (remplie automatiquement à l’insertion dans la base).
-# Ne se met PAS à jour par la suite.
+    
+    # Date et heure de création du post (remplie 
+    # automatiquement à l’insertion dans la base).
+    # Ne se met PAS à jour par la suite.
     created = models.DateTimeField(auto_now_add=True)
-# Date et heure de la dernière modification.
-# Mise à jour automatiquement chaque fois qu’on appelle `save()` sur l’objet
+    
+    # Date et heure de la dernière modification.
+    # Mise à jour automatiquement chaque fois
+    # qu’on appelle `save()` sur l’objet
     updated = models.DateTimeField(auto_now=True)
     
     status = models.CharField(max_length=2,choices=Status,default=Status.DRAFT)
@@ -37,3 +54,11 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[
+            self.published.year,
+            self.published.month,
+            self.published.day,
+            self.slug])
+    
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request,'blog/post/list.html',{'posts':posts})
+
+def post_detail(request,year,month,day,post):
+    # post=Post.objects.get(id=id)
+    post=get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        published__year=year,
+        published__month=month,
+        published__day=day,
+    )
+    return render(request,'blog/post/detail.html',{'post':post})
