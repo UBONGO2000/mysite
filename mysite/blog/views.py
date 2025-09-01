ded9f5dedb46f3d862c43591a5c975f3e7a1b483
UBONGO2000 <gadyubongo@gmail.com>
2025-09-01 23:57:19 +0200

    syteme envoye e-mail partie 1

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,24 +1,37 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+from django.views.generic import ListView
 
 from .models import Post
+from .forms import EmailPostForm
 
 # Create your views here.
 
-def post_list(request):
-    post_list = Post.publish.all()
-    paginator=Paginator(post_list,3)
-    page_number=request.GET.get('page',1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # Si page_number n'est pas un nombre entier, récupérer la première page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page_number is out of range get last page of results
-        posts = paginator.page(paginator.num_pages)
+
+class PostListView(ListView):
+    queryset=Post.publish.all()
+    context_object_name='posts'
+    paginate_by=3
+    template_name='blog/post/list.html'
     
-    return render(request,'blog/post/list.html',{'posts':posts})
+    
+    
+
+
+# def post_list(request):
+#     post_list = Post.publish.all()
+#     paginator=Paginator(post_list,3)
+#     page_number=request.GET.get('page',1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # Si page_number n'est pas un nombre entier, récupérer la première page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page_number is out of range get last page of results
+#         posts = paginator.page(paginator.num_pages)
+    
+    # return render(request,'blog/post/list.html',{'posts':posts})
 
 def post_detail(request,year,month,day,post):
     # post=Post.objects.get(id=id)
@@ -31,3 +44,33 @@ def post_detail(request,year,month,day,post):
         published__day=day,
     )
     return render(request,'blog/post/detail.html',{'post':post})
+
+
+
+def post_share(request, post_id):
+    
+    post=get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    
+    
+    if request.method == 'POST':
+        
+        form = EmailPostForm(request.POST)
+        
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post':post,
+            'form':form    
+        }
+    )
+    
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,3 +121,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from decouple import config
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
