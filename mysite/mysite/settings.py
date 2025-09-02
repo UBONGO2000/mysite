ce425b029fe7f45a261e0d4137c6a84ec89c0d31
UBONGO2000 <gadyubongo@gmail.com>
2025-09-02 12:35:08 +0200

    configuration envoie e-mail part2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -131,5 +131,3 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
